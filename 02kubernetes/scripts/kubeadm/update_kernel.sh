2966b28b943ca14a4aa0d69e9d19213e856b7340
zhangjunyong <zhangjunyong@pdmi.cn>
2019-06-05 17:44:31 +0800

    update scripts

--- 02kubernetes/scripts/kubeadm/update_kernel.sh
+++ 02kubernetes/scripts/kubeadm/update_kernel.sh
@@ -4,7 +4,7 @@ rpm -Uvh https://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
 
 # 查看最新的操作系统内核版本，这里版本为5.0.9-1.el7.elrepo
 # yum --disablerepo="*" --enablerepo="elrepo-kernel" list available
-yum --disablerepo="*" --enablerepo="elrepo-kernel" install -y kernel-ml-5.0.9-1.el7.elrepo
+yum --disablerepo="*" --enablerepo="elrepo-kernel" install -y kernel-ml-5.1.1-1.el7.elrepo
 
 # 修改/etc/default/grub的启动选项
 cp -f /root/kubeadm/kernel/grub /etc/default/grub
